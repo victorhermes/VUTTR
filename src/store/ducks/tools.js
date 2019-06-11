a28f48a206dc234f0ee93ff917b2ca07f3e6cdf9
Victor Hermes <me.victorhermes@gmail.com>
2019-06-10 23:20:23 -0300

    trash removed

--- src/store/ducks/tools.js
+++ src/store/ducks/tools.js
@@ -18,8 +18,6 @@ const { Types, Creators } = createActions({
   editToolByIdSuccess: ['tool'],
   openAddToolModal: null,
   closeAddToolModal: null,
-  openEditToolModal: null,
-  closeEditToolModal: null,
 });
 
 export const ToolsTypes = Types;
@@ -30,8 +28,6 @@ export default Creators;
 export const INITIAL_STATE = Immutable({
   tool: {},
   data: [],
-  openAddToolModal: false,
-  openEditToolModal: false,
 });
 
 /* Reducers */
@@ -43,10 +39,6 @@ export const success = (state, { data }) => state.merge({ data });
 export const openAddModal = state => state.merge({ openAddToolModal: true });
 
 export const closeAddModal = state => state.merge({ openAddToolModal: false });
-
-export const openEditModal = state => state.merge({ openEditToolModal: true });
-
-export const closeEditModal = state => state.merge({ openEditToolModal: false });
 
 export const deleteSuccess = (state, { id }) => state.merge({
   ...state,
@@ -72,8 +64,6 @@ export const reducer = createReducer(INITIAL_STATE, {
   [Types.GET_TOOL_SUCCESS]: success,
   [Types.OPEN_ADD_TOOL_MODAL]: openAddModal,
   [Types.CLOSE_ADD_TOOL_MODAL]: closeAddModal,
-  [Types.OPEN_EDIT_TOOL_MODAL]: openEditModal,
-  [Types.CLOSE_EDIT_TOOL_MODAL]: closeEditModal,
   [Types.CREATE_TOOL_SUCCESS]: createSuccess,
   [Types.DELETE_TOOL_SUCCESS]: deleteSuccess,
   [Types.EDIT_TOOL_SUCCESS]: editSuccess,
