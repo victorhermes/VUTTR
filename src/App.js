2da879024f4cc12572924f9063b8142e42920d95
Victor Hermes <me.victorhermes@gmail.com>
2019-06-21 22:45:00 -0300

    title

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => (
   <Provider store={store}>
     <Helmet>
       <meta charSet="utf-8" />
-      <title>VUTTR by Victor Hermes</title>
+      <title>VUTTR</title>
       <meta
         name="description"
         content="Save interesting tools in a safe place. From programmers to programmers."
