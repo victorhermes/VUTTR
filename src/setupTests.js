6e75514dbe90581ad0fb7b05fec14ea8b5ac495c
Victor Hermes <me.victorhermes@gmail.com>
2019-07-03 19:36:06 -0300

    tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,7 @@
-import { configure } from 'enzyme';
+import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-configure({ adapter: new Adapter() });
+Enzyme.configure({
+  adapter: new Adapter(),
+  disableLifecycleMethods: true,
+});
