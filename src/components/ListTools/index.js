dc52d4c4d75a9552bfb8fa4e9afde32035d3fb49
Victor Hermes <me.victorhermes@gmail.com>
2019-07-13 23:54:42 -0300

    react hooks

--- src/components/ListTools/index.js
+++ src/components/ListTools/index.js
@@ -1,7 +1,6 @@
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 import IconPlusCircle from '../../assets/images/Icon-Plus-Circle.svg';
 import ToolsActions from '../../store/ducks/tools';
@@ -12,153 +11,116 @@ import {
   Container, ToolSection, ToolHeader, Header, Tags, Search, Btn,
 } from './styles';
 
-class ListTools extends Component {
-  static defaultProps = {
-    tools: PropTypes.shape({
-      data: PropTypes.arrayOf(
-        PropTypes.shape({
-          id: PropTypes.number,
-          title: PropTypes.string,
-          link: PropTypes.string,
-          description: PropTypes.string,
-          tags: PropTypes.array,
-        }),
-      ),
-    }).isRequired,
-  };
+export default function ListTools() {
+  const dispatch = useDispatch();
+  const tools = useSelector(state => state.tools);
 
-  static propTypes = {
-    editToolByIdRequest: PropTypes.func.isRequired,
-    getToolRequest: PropTypes.func.isRequired,
-    getAllToolRequest: PropTypes.func.isRequired,
-    getByTagToolRequest: PropTypes.func.isRequired,
-    openAddToolModal: PropTypes.func.isRequired,
-    openRemoveToolModal: PropTypes.func.isRequired,
-    tools: PropTypes.shape({
-      openAddToolModal: PropTypes.bool,
-      openRemoveToolModal: PropTypes.bool,
-      data: PropTypes.arrayOf(
-        PropTypes.shape({
-          id: PropTypes.number,
-          title: PropTypes.string,
-          link: PropTypes.string,
-          description: PropTypes.string,
-          tags: PropTypes.array,
-        }),
-      ),
-    }),
-  };
+  const [checkTag, setTag] = useState(false);
+  const [id, setId] = useState(false);
 
-  state = {
-    checkTag: false,
-    id: false,
-  };
+  useEffect(() => {
+    dispatch(ToolsActions.getToolRequest());
+    // eslint-disable-next-line
+  }, []);
 
-  componentDidMount() {
-    const { getToolRequest } = this.props;
-    getToolRequest();
+  function deleteTool(e) {
+    const toolId = e.target.value;
+    setId(toolId);
+    dispatch(ToolsActions.openRemoveToolModal());
   }
 
-  deleteTool = (e) => {
-    const { openRemoveToolModal } = this.props;
-    const id = e.target.value;
-    this.setState({ id });
-    openRemoveToolModal();
-  };
-
-  editTool = (e) => {
-    const { openAddToolModal, editToolByIdRequest } = this.props;
-    const id = e.target.value;
-    editToolByIdRequest(id);
-    openAddToolModal();
-  };
-
-  filterTools = (e) => {
-    const { getAllToolRequest } = this.props;
-    const word = e.target.value;
-    getAllToolRequest(word);
-  };
-
-  filterByTagTools = (e) => {
-    const { getByTagToolRequest } = this.props;
-    const word = e.target.value;
-    getByTagToolRequest(word);
-  };
-
-  getCheck = (e) => {
-    const checkTag = e.target.checked;
-    this.setState({ checkTag });
-  };
-
-  render() {
-    const { tools, openAddToolModal } = this.props;
-    const { checkTag, id } = this.state;
-
-    return (
-      <Container>
-        <Header>
-          <Search>
-            <input
-              type="text"
-              placeholder="Procurar ferramenta"
-              onChange={!checkTag ? this.filterTools : this.filterByTagTools}
-            />
-
-            <div>
-              <input type="checkbox" onClick={this.getCheck} />
-              <p>Procurar por tags?</p>
-            </div>
-          </Search>
-          <ModalButton onClick={openAddToolModal}>
-            <img src={IconPlusCircle} alt="Adicionar item" />
-          </ModalButton>
-        </Header>
-
-        {tools.data.length ? (
-          tools.data.map(tool => (
-            <ToolSection key={tool.id}>
-              <ToolHeader>
-                <a target="_blank" rel="noopener noreferrer" href={tool.link}>
-                  <h1> {tool.title}</h1>
-                </a>
-                <div>
-                  <Btn type="button" value={tool.id} onClick={this.editTool}>
-                    ✎
-                  </Btn>
-
-                  <Btn type="button" value={tool.id} onClick={this.deleteTool}>
-                    ✖
-                  </Btn>
-                </div>
-              </ToolHeader>
-
-              <p>{tool.description}</p>
-
-              <Tags>
-                {tool.tags.map(tag => (
-                  <span key={Math.random() + tag.value}>#{tag.value}</span>
-                ))}
-              </Tags>
-            </ToolSection>
-          ))
-        ) : (
-          <h2 align="center">Ferramenta não existe!</h2>
-        )}
-
-        {tools.openAddToolModal && <ModalAdd />}
-        {tools.openRemoveToolModal && <ModalDelete idTool={id} />}
-      </Container>
-    );
+  function editTool(e) {
+    const toolId = e.target.value;
+    dispatch(ToolsActions.editToolByIdRequest(toolId));
+    dispatch(ToolsActions.openAddToolModal());
   }
+
+  function filterTools(e) {
+    const word = e.target.value;
+    dispatch(ToolsActions.getAllToolRequest(word));
+  }
+
+  function filterByTagTools(e) {
+    const word = e.target.value;
+    dispatch(ToolsActions.getByTagToolRequest(word));
+  }
+
+  function getCheck(e) {
+    const checkingTag = e.target.checked;
+    setTag(checkingTag);
+  }
+
+  function openModalAdd() {
+    dispatch(ToolsActions.openAddToolModal());
+  }
+
+  return (
+    <Container>
+      <Header>
+        <Search>
+          <input
+            type="text"
+            placeholder="Procurar ferramenta"
+            onChange={!checkTag ? filterTools : filterByTagTools}
+          />
+
+          <div>
+            <input type="checkbox" onClick={getCheck} />
+            <p>Procurar por tags?</p>
+          </div>
+        </Search>
+        <ModalButton onClick={openModalAdd}>
+          <img src={IconPlusCircle} alt="Adicionar item" />
+        </ModalButton>
+      </Header>
+
+      {tools.data.length ? (
+        tools.data.map(tool => (
+          <ToolSection key={tool.id}>
+            <ToolHeader>
+              <a target="_blank" rel="noopener noreferrer" href={tool.link}>
+                <h1> {tool.title}</h1>
+              </a>
+              <div>
+                <Btn type="button" value={tool.id} onClick={editTool}>
+                  ✎
+                </Btn>
+
+                <Btn type="button" value={tool.id} onClick={deleteTool}>
+                  ✖
+                </Btn>
+              </div>
+            </ToolHeader>
+
+            <p>{tool.description}</p>
+
+            <Tags>
+              {tool.tags.map(tag => (
+                <span key={Math.random() + tag.value}>#{tag.value}</span>
+              ))}
+            </Tags>
+          </ToolSection>
+        ))
+      ) : (
+        <h2 align="center">Ferramenta não existe!</h2>
+      )}
+
+      {tools.openAddToolModal && <ModalAdd />}
+      {tools.openRemoveToolModal && <ModalDelete idTool={id} />}
+    </Container>
+  );
 }
 
-const mapStateToProps = state => ({
-  tools: state.tools,
-});
-
-const mapDispatchToProps = dispatch => bindActionCreators(ToolsActions, dispatch);
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(ListTools);
+ListTools.defaultProps = {
+  tools: PropTypes.shape({
+    data: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number,
+        title: PropTypes.string,
+        link: PropTypes.string,
+        description: PropTypes.string,
+        tags: PropTypes.array,
+      }),
+    ),
+  }).isRequired,
+};
